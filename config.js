ddc6115bd7b59296f408c38464a6c842a3f9cf1b
zazazaxky-m <78693013+zazazaxky-m@users.noreply.github.com>
2023-05-24 12:22:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,54 +1,85 @@
 module.exports = {
-TOKEN: "",
-ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
-botInvite: "", //write your discord bot invite.
-supportServer: "", //write your discord bot support server invite.
-mongodbURL: "", //write your mongodb url.
-status: '❤️ codeshare.me',
-commandsDir: './commands', //Please don't touch
-language: "en", //en, tr, nl, pt, fr, ar, zh_TW, it, ja
-embedColor: "ffa954", //hex color code
-errorLog: "", //write your discord error log channel id.
+  TOKEN:
+    "MTA4Mzc2NDUyMDgxMzYxMzA1Ng.Gf7rVM.lVLFSFlqfe_H6cfOoyZK2TC2NoN3V8oZU0bHGc",
+  ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
+  botInvite:
+    "https://discord.com/oauth2/authorize?client_id=1083764520813613056&scope=bot&permissions=8", //write your discord bot invite.
+  supportServer: "", //write your discord bot support server invite.
+  mongodbURL: "mongodb+srv://zacky0204:zacky0204@cluster0.hvclvxh.mongodb.net", //write your mongodb url.
+  status: "Made With ❤️ by Z",
+  commandsDir: "./commands", //Please don't touch
+  language: "id", //en, tr, nl, pt, fr, ar, zh_TW, it
+  embedColor: "ffa954", //hex color code
+  errorLog: "", //write your discord error log channel id.
 
+  sponsor: {
+    status: true, //true or false
+    url: "https://awmbilisim.com", //write your discord sponsor url.
+  },
 
-sponsor: {
-status: true, //true or false
-url: "https://awmbilisim.com", //write your discord sponsor url.
-},
+  voteManager: {
+    //optional
+    status: false, //true or false
+    api_key: "", //write your top.gg api key.
+    vote_commands: [
+      "back",
+      "channel",
+      "clear",
+      "dj",
+      "filter",
+      "loop",
+      "nowplaying",
+      "pause",
+      "play",
+      "playlist",
+      "queue",
+      "resume",
+      "save",
+      "search",
+      "skip",
+      "stop",
+      "time",
+      "volume",
+    ], //write your use by vote commands.
+    vote_url: "", //write your top.gg vote url.
+  },
 
-voteManager: { //optional
-status: false, //true or false
-api_key: "", //write your top.gg api key. 
-vote_commands: ["back","channel","clear","dj","filter","loop","nowplaying","pause","play","playlist","queue","resume","save","search","skip","stop","time","volume"], //write your use by vote commands.
-vote_url: "", //write your top.gg vote url.
-},
+  shardManager: {
+    shardStatus: false, //If your bot exists on more than 1000 servers, change this part to true.
+  },
 
-shardManager:{
-shardStatus: false //If your bot exists on more than 1000 servers, change this part to true.
-},
+  playlistSettings: {
+    maxPlaylist: 10, //max playlist count
+    maxMusic: 75, //max music count
+  },
 
-playlistSettings:{
-maxPlaylist: 10, //max playlist count
-maxMusic: 75, //max music count
-},
+  opt: {
+    DJ: {
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "skip",
+        "stop",
+        "volume",
+        "shuffle",
+      ], //Please don't touch
+    },
 
-opt: {
-DJ: {
-commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle'] //Please don't touch
-},
+    voiceConfig: {
+      leaveOnFinish: false, //If this variable is "true", the bot will leave the channel the music ends.
+      leaveOnStop: false, //If this variable is "true", the bot will leave the channel when the music is stopped.
 
-voiceConfig: {
-leaveOnFinish: false, //If this variable is "true", the bot will leave the channel the music ends.
-leaveOnStop: false, //If this variable is "true", the bot will leave the channel when the music is stopped.
+      leaveOnEmpty: {
+        //The leaveOnEnd variable must be "false" to use this system.
+        status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
+        cooldown: 10000000, //1000 = 1 second
+      },
+    },
 
-leaveOnEmpty: { //The leaveOnEnd variable must be "false" to use this system.
-status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
-cooldown: 10000000, //1000 = 1 second
-},
-
-},
-
-maxVol: 150, //You can specify the maximum volume level.
-
-}
-}
+    maxVol: 150, //You can specify the maximum volume level.
+  },
+};
